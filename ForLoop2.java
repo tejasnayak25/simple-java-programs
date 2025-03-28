a911bebbed7ba56a07c9df0a6607b39e71122029
Tejas Nayak <arpithanayak900@gmail.com>
2025-03-28 11:48:55 +0530

    update

--- ForLoop2.java
+++ ForLoop2.java
@@ -3,5 +3,12 @@ public class ForLoop2 {
         for(int i=1;i<6;i++) {
             System.out.print(i + " ");
         }
+        System.out.println();
+        int n=5;
+        char ch='a';
+        for(int j=0;j<n;j++) {
+            System.out.print(ch+" ");
+            ch++;
+        }
     }
 }
