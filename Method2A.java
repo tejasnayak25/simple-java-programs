065f2c4b30ebad513d2886356cd09d18c4b5f5bc
Tejas Nayak <arpithanayak900@gmail.com>
2025-03-28 12:55:33 +0530

    update

---
+++ Method2A.java
@@ -0,0 +1,10 @@
+public class Method2A {
+    static void multiply(int a, int b, int c) {
+        System.out.println(a*b*c);
+    }
+    public static void main(String[] args) {
+        System.out.println("Program Starts");
+        multiply(1, 2, 3);
+        System.out.println("Program Ends");
+    }
+}
