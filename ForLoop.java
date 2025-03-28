9312d301413d30004a73f4c6b2f9f0f2e7c43f97
Tejas Nayak <arpithanayak900@gmail.com>
2025-03-28 11:44:23 +0530

    more programs

--- ForLoop.java
+++ ForLoop.java
@@ -7,5 +7,13 @@ public class ForLoop {
         for(int x=5;x>0;x--) {
             System.out.println("Python");
         }
+
+        for(int m=0;m<10;m=m+2) {
+            System.out.println("Bangalore");
+        }
+
+        for(int n=10;n>0;n=n-3) {
+            System.out.println("Chennai");
+        }
     }
 }
