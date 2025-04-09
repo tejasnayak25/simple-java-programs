02342107397de81dcd9428e3dd867a6d37bde6aa
Tejas Nayak <arpithanayak900@gmail.com>
2025-04-09 11:51:46 +0530

    update

---
+++ Methods.java
@@ -0,0 +1,27 @@
+public class Methods {
+    static void greetings() {
+        System.out.println("Hello World");
+    }
+
+    static void mul(int i, int j, int k) {
+        System.out.println(i*j*k);
+    }
+
+    static String name() {
+        return "Uma";
+    }
+
+    static boolean flag(int i) {
+        if(i % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        greetings();
+        mul(2, 3, 4);
+        System.out.println(name());
+        System.out.println(flag(3));
+    }
+}
