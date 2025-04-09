e60d9899e061b08b2db3df54e421d2819ac27ddd
Tejas Nayak <arpithanayak900@gmail.com>
2025-04-09 12:54:57 +0530

    update

---
+++ Array3.java
@@ -0,0 +1,10 @@
+// print the elements in an array
+public class Array3 {
+    public static void main(String[] args) {
+        int[] arr = {11,12,13,14,15};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
