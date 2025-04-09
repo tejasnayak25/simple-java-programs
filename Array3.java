76b61595d64c8e3bcb8b02b1550db6fcc362b699
Tejas Nayak <arpithanayak900@gmail.com>
2025-04-09 12:58:37 +0530

    update

--- Array3.java
+++ Array3.java
@@ -6,5 +6,9 @@ public class Array3 {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
     }
 }
