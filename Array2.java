a55940dffbcdc1b74bdd65f4706ccbe5a636b163
Tejas Nayak <arpithanayak900@gmail.com>
2025-04-09 12:51:01 +0530

    update

---
+++ Array2.java
@@ -0,0 +1,14 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4,5};
+
+        System.out.println(n.length);
+
+        double[] heights = {5.3, 4.2, 5.9};
+
+        System.out.println(heights.length);
+
+        char[] ch = {'e', 'g', 'h', 'g'};
+        System.out.println(ch.length);
+    }
+}
