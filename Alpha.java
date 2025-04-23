664833df1a5368f28fbb0fdd73090f9c4a8bdeaa
Tejas Nayak <arpithanayak900@gmail.com>
2025-04-23 11:49:24 +0530

    update

---
+++ Alpha.java
@@ -0,0 +1,21 @@
+public class Alpha {
+    int n;
+    double d;
+    boolean b;
+    String name;
+    char ch;
+
+    /*
+     * Default constructor
+     * n = 0;
+     * d = 0.0;
+     * b = false;
+     * name = null
+     * ch = ;
+     */
+
+    public static void main(String[] args) {
+        Alpha a = new Alpha();
+        System.out.println(a.n + " " + a.d + " " + a.b + " " + a.name + " " + a.ch);
+    }
+}
