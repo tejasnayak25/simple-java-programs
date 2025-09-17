8c1f32ed2f74142a6b8a5be7fd2fb5d2e2fe8731
Tejas Nayak <arpithanayak900@gmail.com>
2025-09-17 11:44:26 +0530

    update

--- day3/ASCII.java
+++ day3/ASCII.java
@@ -6,9 +6,23 @@ public class ASCII {
             System.out.println(i+"->"+(char)i);
         }
 
-        String x = "Mister X";
+        String x = "Mister 123";
+        int c = 0, s = 0, sp = 0, d = 0;
         for(int i=0;i<x.length();i++) {
-            System.out.println(x.charAt(i));
+            char ch = x.charAt(i);
+            if(ch >= 65 && ch <= 90)
+                c++;
+            else if(ch >= 97 && ch <= 122)
+                s++;
+            else if(ch == 32)
+                sp++;
+            else if(ch >= 48 && ch <= 57)
+                d++;
         }
+
+        System.out.println("Uppercase: "+c);
+        System.out.println("Lowercase: "+s);
+        System.out.println("Spaces: "+sp);
+        System.out.println("Digits: "+d);
     }
 }
