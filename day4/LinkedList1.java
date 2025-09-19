f98eeff178ddcef2c2ace32dbc820363a18dd324
Tejas Nayak <arpithanayak900@gmail.com>
2025-09-19 11:50:53 +0530

    update

---
+++ day4/LinkedList1.java
@@ -0,0 +1,13 @@
+package day4;
+
+import java.util.*;
+
+public class LinkedList1 {
+    public static void main(String[] args) {
+        List<Integer> l = new LinkedList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        System.out.println(l);
+    }
+}
