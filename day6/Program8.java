9bcd4dccb7682f60f932d99a91493fff464cc634
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-03 12:02:01 +0530

    update

---
+++ day6/Program8.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class Program8 {
+    public static void main(String[] args) {
+        System.out.println(call(1,10,0));
+    }
+
+    public static int call(int s, int e, int sum) {
+        if(s==e+1) return sum;
+        else {
+            if(s%2==0){
+                sum = sum+s;
+            }
+            return call(s+1, e, sum);
+        }
+    }
+}
