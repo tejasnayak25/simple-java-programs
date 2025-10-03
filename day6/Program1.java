e1a6cce01be0ac6c089979582b2d305a70b967f4
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-03 11:32:15 +0530

    update

---
+++ day6/Program1.java
@@ -0,0 +1,15 @@
+public class Program1 {
+    public static void main(String[] args) {
+        call(1,10);
+    }
+
+    public static void call(int s, int e) {
+        if(s==e+1)
+            return;
+        else
+        {
+            System.out.println(s);
+            call(s+1, e);
+        }
+    }
+}
