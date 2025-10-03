17ba72addee635d6ea75aee56d761df923246636
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-03 11:41:46 +0530

    update

---
+++ day6/Program3.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class Program3 {
+    public static void main(String[] args) {
+        call(1, 10);
+    }
+    public static void call(int s, int e) {
+        if(s==e+1) return;
+        else {
+            if(s%2==0)
+                System.out.println(s);
+            call(s+1, e);
+        }
+    }
+}
