7e56add3309bc1ea1eaf9ced87092450c1ea110c
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-15 12:42:56 +0530

    update

---
+++ day7/Array4.java
@@ -0,0 +1,33 @@
+package day7;
+
+public class Array4 {
+    public static void main(String[] args) {
+        int arr[][][] = {
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            },
+            {
+                {11,12,13},
+                {14,15,16},
+                {17,18,19}
+            },
+            {
+                {21,22,23},
+                {24,25,26},
+                {27,28,29}
+            }
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.print(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
