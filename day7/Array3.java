fb275744967c5b0fc9be93e0b0f664967cfd9476
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-15 12:30:03 +0530

    update

--- day7/Array3.java
+++ day7/Array3.java
@@ -4,12 +4,26 @@ public class Array3 {
     public static void main(String[] args) {
         int arr[][] = {
             {1,2,3},
-            {4,5},
+            {4,5,6},
             {7,8,9}
         };
         
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = arr[i].length - 1; j >= 0; j--) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
