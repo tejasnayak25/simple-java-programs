6b84edf3bb55a34bb2d9de622c8f5570948bac07
Tejas Nayak <arpithanayak900@gmail.com>
2025-10-15 12:21:47 +0530

    update

--- day7/Array3.java
+++ day7/Array3.java
@@ -4,7 +4,7 @@ public class Array3 {
     public static void main(String[] args) {
         int arr[][] = {
             {1,2,3},
-            {4,5,6},
+            {4,5},
             {7,8,9}
         };
         
