05d6b9349d507331e397cef173d99bcbd0d25840
Tejas Nayak <arpithanayak900@gmail.com>
2025-11-05 12:47:51 +0530

    update

---
+++ day10/InsertionSort.java
@@ -0,0 +1,20 @@
+package day10;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = { 40, 30, 10, 20 };
+
+        for (int i = 1; i < arr.length; i++) {
+            while (i > 0 && arr[i] < arr[i - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
+                i--;
+            }
+        }
+        
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
